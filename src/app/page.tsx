ebf9db53fb1fdd711d07778c52ca25bb58a52647
jakubpatzer <jakub.patzer@protonmail.com>
2024-07-29 14:48:25 +0200

    Added big hud mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import Pagination from "@/components/Pagination";
 import Modal from "@/components/Modal";
+import Switch from "@/components/Switch";
 import generatePDF from "@/utils/generatePDF";
 
 interface DataItem {
@@ -34,19 +35,12 @@ const PaginatedPage = () => {
   const [showChecked, setShowChecked] = useState<boolean>(false);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [modalVersion, setModalVersion] = useState<"reset" | "pdf">("reset");
+  const [isBigHud, setIsBigHud] = useState<boolean>(false);
 
-  // Save data to localStorage whenever it changes
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data));
-  }, [data]);
-
-  // Ensure data is loaded from localStorage on component mount
-  useEffect(() => {
-    const storedData = localStorage.getItem(LOCAL_STORAGE_KEY);
-    if (storedData) {
-      setData(JSON.parse(storedData));
-    }
-  }, []);
+  const bigHud = {
+    on: 'text-2xl',
+    off: 'text-md'
+  }
 
   const totalPages: number = data.length;
 
@@ -90,6 +84,7 @@ const PaginatedPage = () => {
     localStorage.removeItem(LOCAL_STORAGE_KEY);
     setCurrentPage(1);
     setIsModalOpen(false);
+    setShowChecked(false);
   };
 
   const handleCancelReset = (): void => {
@@ -109,11 +104,28 @@ const PaginatedPage = () => {
     }
   };
 
+  // Save data to localStorage whenever it changes
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data));
+  }, [data]);
+
+  // Ensure data is loaded from localStorage on component mount
+  useEffect(() => {
+    const storedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (storedData) {
+      setData(JSON.parse(storedData));
+    }
+  }, []);
+
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4 text-center text-white">
         Speed dating helper
       </h1>
+      <div className="flex items-center justify-between mb-4 px-16">
+        <span className="text-white">Tryb Big HUD</span>
+        <Switch isChecked={isBigHud} onChange={setIsBigHud} />
+      </div>
       <ul>
         {currentData.map((item: DataItem) => (
           <li key={item.id} className="border rounded-lg py-2 px-2 mb-2">
@@ -127,7 +139,7 @@ const PaginatedPage = () => {
               />
             </div>
             <textarea
-              className="w-full border rounded p-2 text-black"
+              className={`${isBigHud ? bigHud.on: bigHud.off} w-full border rounded p-2 text-black`}
               placeholder="Notatki..."
               value={item.notes}
               onChange={(e) => handleNotesChange(item.id, e.target.value)}
@@ -139,22 +151,23 @@ const PaginatedPage = () => {
         currentPage={currentPage}
         totalPages={totalPages}
         onPageChange={handlePageChange}
+        isBigHud={isBigHud}
       />
       <div className="flex flex-col mt-4 md:flex-row md:justify-center md:space-x-4">
         <button
-          className="w-full md:w-auto bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded mb-4 md:mb-0"
+          className={`${isBigHud ? bigHud.on: bigHud.off} w-full md:w-auto bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded mb-4 md:mb-0`}
           onClick={handleReset}
         >
           Resetuj wszystko
         </button>
         <button
-          className="w-42 md:w-auto bg-green-600 hover:bg-green-700  text-white py-2 px-4 mb-4 rounded"
+          className={`${isBigHud ? bigHud.on: bigHud.off} w-42 md:w-auto bg-green-600 hover:bg-green-700  text-white py-2 px-4 mb-4 rounded`}
           onClick={HandleDownloadPDF}
         >
           Pobierz PDF z listą
         </button>
         <button
-          className="w-full md:w-auto bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded mb-4 md:mb-0"
+          className={`${isBigHud ? bigHud.on: bigHud.off} w-full md:w-auto bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded mb-4 md:mb-0`}
           onClick={handleShowChecked}
         >
           {showChecked ? "Ukryj listę" : "Pokaz listę"}
@@ -166,7 +179,7 @@ const PaginatedPage = () => {
           {checkedItems.length > 0 ? (
             <ul>
               {checkedItems.map((item: DataItem) => (
-                <li className="text-white text-xl mb-2" key={item.id}>
+                <li className={`${isBigHud ? bigHud.on: bigHud.off} text-white mb-4`} key={item.id}>
                   {item.name}
                 </li>
               ))}
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -5,10 +5,15 @@ interface PaginationProps {
   currentPage: number;
   totalPages: number;
   onPageChange: (page: number) => void;
+  isBigHud: boolean;
 }
 
-const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageChange }) => {
+const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageChange, isBigHud }) => {
   const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+  const bigHud = {
+    on: 'text-2xl w-12 h-12',
+    off: 'text-lg w-10 h-10'
+  }
 
   return (
     <div className="flex justify-center mt-4 flex-wrap">
@@ -16,9 +21,9 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPage
         <button
           key={page}
           onClick={() => onPageChange(page)}
-          className={`m-2 px-3 py-1 w-10 h-10 border rounded text-lg flex items-center justify-center ${
+          className={`m-2 px-3 py-1 border rounded flex items-center justify-center ${
             page === currentPage ? 'bg-blue-500 text-white' : 'bg-white text-blue-500'
-          }`}
+          } ${isBigHud ? bigHud.on : bigHud.off}`}
         >
           {page}
         </button>
